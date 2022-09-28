a430eef2e096e8e81d84ed11ce79c96b532669ca
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 19:47:25 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "de26834f-79f0-40be-84cf-fb249c93e075",
+    "home_feed": [
+        {
+            "id": "ad74b97c-5fe8-48ae-bb5e-ecc1c961d970",
+            "artist_name": "world",
+            "home_carousel": [
+                {
+                    "id": "658a7829-ecf6-4b8a-9604-18a3641f1bf5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/71.png"
+                },
+                {
+                    "id": "616e5224-9894-46f8-af71-fa12b2853a21",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/25.png"
+                },
+                {
+                    "id": "359cb39a-2fcc-4771-9cf3-08616f66adf7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/233.png"
+                },
+                {
+                    "id": "0a8e1e57-7235-4a3c-a623-331111e11720",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "aa5d5d55-9034-48c1-aa36-65e03fe94ced",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/245.png"
+                }
+            ],
+            "art_caption": "be",
+            "art_description": "South bill light practice example."
+        },
+        {
+            "id": "8239e729-189e-49d8-9f7b-e8ffeb016726",
+            "artist_name": "measure",
+            "home_carousel": [
+                {
+                    "id": "3812ffa4-e2f3-44d2-83b5-cdb4987ae2eb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png"
+                },
+                {
+                    "id": "3dba8e11-40ce-48f1-be23-335ee6fc688c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+                },
+                {
+                    "id": "fd4148a3-637e-491d-80ee-5b816e09da9a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/124.png"
+                },
+                {
+                    "id": "adbf3202-6bf0-41ef-964b-933112fbdcca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                },
+                {
+                    "id": "eefa8f23-2db8-4e2c-bf4d-1e9aa5088f7f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/119.png"
+                }
+            ],
+            "art_caption": "enjoy",
+            "art_description": "Card nothing real phone per. Choice above class certain. Road camera law door dog present purpose."
+        },
+        {
+            "id": "e0612993-b104-4814-8bba-30b7336544bb",
+            "artist_name": "tell",
+            "home_carousel": [
+                {
+                    "id": "4c612a1a-6873-4d44-a915-9a3beed9aff2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png"
+                },
+                {
+                    "id": "15a8179d-e0ea-4900-8d0d-9954cc8a17ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png"
+                },
+                {
+                    "id": "50834c97-ef52-443a-be6f-d08c7b478989",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/47.png"
+                },
+                {
+                    "id": "6c4f0012-774a-41c9-98fd-0356a341c5a0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/275.png"
+                },
+                {
+                    "id": "836f7a2b-e698-498a-a4df-c827261ce6dd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                }
+            ],
+            "art_caption": "close",
+            "art_description": "By onto food feeling Mrs marriage both. Leave social play study."
+        },
+        {
+            "id": "23a49832-25fe-4212-9be0-0f97d0ac51d7",
+            "artist_name": "let",
+            "home_carousel": [
+                {
+                    "id": "694a5ea6-7a5a-4c9d-95a5-d4b82e224968",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "f3c67056-4fa9-4b4f-a802-ac86a5116194",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                },
+                {
+                    "id": "f48bd5ee-8be2-41fa-9346-729187d1a52e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                },
+                {
+                    "id": "aa092a3b-6b71-4532-8881-1883bcbb1786",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png"
+                },
+                {
+                    "id": "fb8382b6-a124-47a1-a8e2-d5ea41258b59",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/351.png"
+                }
+            ],
+            "art_caption": "appear",
+            "art_description": "High skin apply either wrong open item. Compare large agree."
+        },
+        {
+            "id": "d050b587-44ea-46e3-9295-2da93d49daf6",
+            "artist_name": "simply",
+            "home_carousel": [
+                {
+                    "id": "658dac06-bb2d-4d69-8306-e0573e924ca5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/313.png"
+                },
+                {
+                    "id": "92699b39-52eb-4111-a463-45b23d0c00d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/82.png"
+                },
+                {
+                    "id": "fd3e7054-8b2c-4625-a1da-749b84d46046",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png"
+                },
+                {
+                    "id": "5c2bfd98-dd55-434b-a030-d600d46de92f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+                },
+                {
+                    "id": "2d49a0d6-8881-4f7d-a258-5e2a3f2511d0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                }
+            ],
+            "art_caption": "special",
+            "art_description": "Stand civil player music important agent. Tough bed fund pick local decade four."
+        }
+    ]
+}
